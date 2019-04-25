b946a9f51ce372475a886969fa1def46f83367ac
finnishfox <onefinnishfox@gmail.com>
2019-04-25 13:31:35 +0200

    deploy

---
+++ precache-manifest.86ff363fd2f38737e6fb3a5e6a4a37e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5f76ed7485d74de393d6",
+    "url": "./static/js/main.7863fe7e.chunk.js"
+  },
+  {
+    "revision": "969983b5e6b5f5287510",
+    "url": "./static/js/2.3ade9afd.chunk.js"
+  },
+  {
+    "revision": "5f76ed7485d74de393d6",
+    "url": "./static/css/main.40dde2c6.chunk.css"
+  },
+  {
+    "revision": "ca8350002d35c03b51849093882d01cf",
+    "url": "./index.html"
+  }
+];
